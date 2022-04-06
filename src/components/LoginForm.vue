1cf83a55883784ffed977f04b6eada28ca729188
Edu <edu@edu4java.com>
2022-04-06 15:47:16 +0100

    login with components example

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Login</h1>
+  <form @submit.prevent="submit">
+    <login-input v-model="mail" label="Correo" />
+    <login-input v-model="psw" label="Clave" />
+
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import LoginInput from "./LoginInput.vue";
+
+export default {
+  name: "LoginForm",
+  data: () => ({ mail: "edu@edu.com", psw: "ppp" }),
+  methods: {
+    submit() {
+      console.log(`submit ${this.mail} ${this.psw} `);
+    },
+  },
+  components: { LoginInput },
+};
+</script>
+
+
---
+++ src/components/LoginInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <label>
+    {{ label }}
+    <input type="text" v-model="inputValue" />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "LoginInput",
+  props: ['label', 'modelValue'], 
+  computed: {
+    inputValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue",value);
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+input {
+  display: block;
+}
+</style>
